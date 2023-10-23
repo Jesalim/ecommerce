9a030650209de526aee6f3452719f82bf7e0f0bc
Jesalim <jessyculture@gmail.com>
2023-10-23 19:59:32 +0300

    build

---
+++ build/precache-manifest.bd73ae79a00115aff04f851e5c892532.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac1a05841a1c9ec0ad9fb51201c88bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "344b59af5c37bdb719b0",
+    "url": "/static/css/2.79455ca0.chunk.css"
+  },
+  {
+    "revision": "a06f4549ddb7becfe91c",
+    "url": "/static/css/main.1fb11458.chunk.css"
+  },
+  {
+    "revision": "344b59af5c37bdb719b0",
+    "url": "/static/js/2.988e883f.chunk.js"
+  },
+  {
+    "revision": "fc067e8abe54824a406678f539a112f4",
+    "url": "/static/js/2.988e883f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a06f4549ddb7becfe91c",
+    "url": "/static/js/main.7bfb2704.chunk.js"
+  },
+  {
+    "revision": "f64bbedbc980241ccee0",
+    "url": "/static/js/runtime-main.18399cad.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
